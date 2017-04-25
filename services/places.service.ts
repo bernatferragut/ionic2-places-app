9ab394c50d670409e228d0674803c1d6e7a71968
Bernat Ferragut <elbernat@gmail.com>
2017-04-25 08:28:42 -0300

    injecting a persistent storage package

--- services/places.service.ts
+++ services/places.service.ts
@@ -1,13 +1,24 @@
+// Permanent Data Storage injected to this Service
+import { Storage } from '@ionic/storage';
 
 export class PlacesService {
 
     private places: {title: string}[] = [];
 
+    constructor ( private storage: Storage ) { }
+
     AddPlace(place: {title:string}) {
         this.places.push(place);
+        this.storage.set('places', this.places);
     }
 
     getPlaces() {
-        return this.places.slice();
+        return this.storage.get('places')
+            .then(
+                (places) => {
+                    this.places = places;
+                    return this.places.slice();
+                }
+            );
     }
 }
