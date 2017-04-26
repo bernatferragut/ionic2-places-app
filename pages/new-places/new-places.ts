f0688bd21fc258e070f7894d7457f0bc085f0790
Bernat Ferragut <elbernat@gmail.com>
2017-04-26 10:20:05 -0300

    Geolocation added

--- pages/new-places/new-places.ts
+++ pages/new-places/new-places.ts
@@ -1,6 +1,7 @@
 import { NavController } from 'ionic-angular';
 import { Component } from '@angular/core';
 import { PlacesService } from './../../services/places.service';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-new-places',
@@ -8,10 +9,19 @@ import { PlacesService } from './../../services/places.service';
 })
 export class NewPlaces {
 
-  constructor( private placesService: PlacesService, private navCtrl: NavController) { }
+  constructor(private placesService: PlacesService, private navCtrl: NavController, private geolocation: Geolocation) { }
 
   onAddPlace(value: {title: string}){
     this.placesService.AddPlace(value);
     this.navCtrl.pop();
   }
+
+  onLocateUser(){
+    this.geolocation.getCurrentPosition()
+      .then((location) => {
+        console.log('Location fetched successfully') 
+    }).catch((error) => {
+          console.log('Error getting location', error);
+    });
+    }
 }
